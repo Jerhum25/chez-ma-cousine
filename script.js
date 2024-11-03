bcbd0d174dc856f8f079076f85d17780501c1b41
jerhum <jerhum@msn.com>
2024-11-03 08:14:20 +0100

    ajout section carte

---
+++ script.js
@@ -0,0 +1,11 @@
+// const conceptContent = document.querySelector(".conceptContent");
+// const concept = document.querySelector(".concept");
+// const conceptHeight = conceptContent.clientHeight;
+// // conceptContent.addEventListener("onChange", )
+// // console.log(window);
+
+// window.addEventListener("resize", () => {
+//   concept.style.height = `100%`;
+// // console.log(conceptContent.clientHeight);
+
+// });
